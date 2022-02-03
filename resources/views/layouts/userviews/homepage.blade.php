583bdddb640d9a3e6760d093e38f77215f031f08
ah-rasel <18103234@iubat.edu>
2022-02-03 16:25:47 +0600

    How we work - tailwind

--- resources/views/layouts/userviews/homepage.blade.php
+++ resources/views/layouts/userviews/homepage.blade.php
@@ -2,68 +2,13 @@
 
 @section('content')
 {{--
-    @livewire('how-we-work-section')
     @livewire('all-time-hit-brands')
     --}}
     
     <!-- Slider Section -->
-@livewire('homepage-sliders')
-<div class="space-y-5 mt-4">
-    <div class="text-center">
-        <p class="text-gray-500 font-semibold tracking-widest">How we work ?</p>
-        <h2 class="font-bold text-3xl text-gray-800">We work in three major steps</h2>
-    </div>
-    <div class="w-full max-w-6xl mx-auto">
-        <div class="grid grid-cols-1 md:grid-cols-3 gap-x-4">
-            <div class="group">
-                <div class="w-full space-y-5 p-8 box-border border border-gray-100 shadow-sm group-hover:shadow-lg group-hover:cursor-pointer">
-                    <div class="h-12 w-12 flex items-center justify-center bg-gray-100 group-hover:bg-blue-400 shadow mx-auto rounded">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-blue-400 group-hover:text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
-                        </svg>
-                    </div>
-                    <div class="space-y-1">
-                        <h2 class="text-center text-xl font-bold">Choose a Location</h2>
-                        <p class="text-justify text-gray-500">Choose a Location where you want to visit . It can be
-                            anywhere based on your desire .
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="group">
-                <div class="w-full space-y-5 p-8 box-border border border-gray-100 shadow-sm group-hover:shadow-lg group-hover:cursor-pointer">
-                    <div class="h-12 w-12 flex items-center justify-center bg-gray-100 group-hover:bg-blue-400 shadow mx-auto rounded">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-blue-400 group-hover:text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                        </svg>
-                    </div>
-                    <div class="space-y-1">
-                        <h2 class="text-center text-xl font-bold">Set a pick-up date</h2>
-                        <p class="text-justify text-gray-500">Choose a Location where you want to visit . It can be
-                            anywhere based on your desire .
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="group">
-                <div class="w-full space-y-5 p-8 box-border border border-gray-100 shadow-sm group-hover:shadow-lg group-hover:cursor-pointer">
-                    <div class="h-12 w-12 flex items-center justify-center bg-gray-100 group-hover:bg-blue-400 shadow mx-auto rounded">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-blue-400 group-hover:text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" />
-                        </svg>
-                    </div>
-                    <div class="space-y-1">
-                        <h2 class="text-center text-xl font-bold">Book your car</h2>
-                        <p class="text-justify text-gray-500">Choose a Location where you want to visit . It can be
-                            anywhere based on your desire .
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+    @livewire('homepage-sliders')
+    @livewire('how-we-work-section')
+
 <!-- Best Services -->
 <div class="w-full my-5 max-w-6xl mx-auto ">
     <div class="grid grid-cols-2 gap-x-4">
--- resources/views/livewire/how-we-work-section.blade.php
+++ resources/views/livewire/how-we-work-section.blade.php
@@ -1,97 +1,56 @@
-<div>
-<div class="mt-md-2 mt-0 p-3 pb-md-2">
-    <div class="container">
-        <h5 class="text-center p-0 m-0 text-gray-manual">How we work ?</h5>
-        <h3 class="text-center working-steps-heading font-weight-bold">We work in three major steps&nbsp;</h3>
-        <div class="row mt-4">
-            <div class="col col-4 col-md-4 text-center d-md-inline p-0 p-md-2">
-                <div class="card working-steps-card m-2">
-                    <div class="card-body">
-                        <div><i class="fa fa-map-marker workin-process-icon"></i>
-                            <h5 class="working-heading text-center">Choose a Location</h5>
-                        </div>
-                        <p class="card-text d-md-inline d-none d-md-inline">Choose a Location where you want to visit . It can be anywhere based on your desire .</p>
+<div class="space-y-5 mt-4">
+    <div class="text-center">
+        <p class="text-gray-500 font-semibold tracking-widest">How we work ?</p>
+        <h2 class="font-bold text-3xl text-gray-800">We work in three major steps</h2>
+    </div>
+    <div class="w-full max-w-6xl mx-auto">
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-x-4">
+            <div class="group">
+                <div class="w-full space-y-5 p-8 box-border border border-gray-100 shadow-sm group-hover:shadow-lg group-hover:cursor-pointer">
+                    <div class="h-12 w-12 flex items-center justify-center bg-gray-100 group-hover:bg-blue-400 shadow mx-auto rounded">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-blue-400 group-hover:text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
+                        </svg>
+                    </div>
+                    <div class="space-y-1">
+                        <h2 class="text-center text-xl font-bold">Choose a Location</h2>
+                        <p class="text-justify text-gray-500">Choose a Location where you want to visit . It can be
+                            anywhere based on your desire .
+                        </p>
                     </div>
                 </div>
             </div>
-            <div class="col col-4 col-md-4 text-center d-md-inline p-0 p-md-2">
-                <div class="card working-steps-card m-2">
-                    <div class="card-body">
-                        <div><i class="fa fa-calendar-check-o workin-process-icon"></i>
-                            <h5 class="working-heading">Set a pick-up date</h5>
-                        </div>
-                        <p class="card-text d-md-inline d-none d-md-inline">Choose a Location where you want to visit . It can be anywhere based on your desire .</p>
+            <div class="group">
+                <div class="w-full space-y-5 p-8 box-border border border-gray-100 shadow-sm group-hover:shadow-lg group-hover:cursor-pointer">
+                    <div class="h-12 w-12 flex items-center justify-center bg-gray-100 group-hover:bg-blue-400 shadow mx-auto rounded">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-blue-400 group-hover:text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                        </svg>
+                    </div>
+                    <div class="space-y-1">
+                        <h2 class="text-center text-xl font-bold">Set a pick-up date</h2>
+                        <p class="text-justify text-gray-500">Choose a Location where you want to visit . It can be
+                            anywhere based on your desire .
+                        </p>
                     </div>
                 </div>
             </div>
-            <div class="col col-4 col-md-4 text-center d-md-inline p-0 p-md-2">
-                <div class="card working-steps-card m-2">
-                    <div class="card-body">
-                        <div><i class="fa fa-bookmark workin-process-icon"></i>
-                            <h5 class="working-heading">Book your car</h5>
-                        </div>
-                        <p class="card-text d-md-inline d-none d-md-inline">Choose a Location where you want to visit . It can be anywhere based on your desire .</p>
+            <div class="group">
+                <div class="w-full space-y-5 p-8 box-border border border-gray-100 shadow-sm group-hover:shadow-lg group-hover:cursor-pointer">
+                    <div class="h-12 w-12 flex items-center justify-center bg-gray-100 group-hover:bg-blue-400 shadow mx-auto rounded">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-blue-400 group-hover:text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" />
+                        </svg>
+                    </div>
+                    <div class="space-y-1">
+                        <h2 class="text-center text-xl font-bold">Book your car</h2>
+                        <p class="text-justify text-gray-500">Choose a Location where you want to visit . It can be
+                            anywhere based on your desire .
+                        </p>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-<div>
-    <div class="container">
-        <div class="row">
-            <div class="col col-md-6 why-us-column-left">
-                <div class="why-us-image"><img class="why-us-image-img d-none d-md-inline" src="{{asset('userFiles/assets/img/pngegg.png')}}"></div>
-            </div>
-            <div class="col col-md-6 col-12">
-                <div>
-                    <div>
-                        <h5 class="text-gray-manual services-text-grey text-center text-md-left">Best Services</h5>
-                        <h3 class="services-text-main text-center text-md-left">Feel the best experience with our special deals</h3>
-                        <hr class="slider-hr p-0 text-sm-center text-center m-md-0">
-                    </div>
-                    <div class="mt-3">
-                        <div class="row">
-                            <div class="col col-2 col-md-3 ml-4 ml-md-0">
-                                <div><i class="fa fa-handshake-o why-us-icon workin-process-icon" style="font-size: 21px;"></i></div>
-                            </div>
-                            <div class="col col-8 col-md-9">
-                                <div>
-                                    <h5 class="why-us-icon-heading working-heading m-0">Deals for every budget</h5>
-                                    <p class="why-us-icon-paragraph working-steps-text pt-1">Incredible prices on hotels, cars , flights and packages worldwide</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="mt-3">
-                        <div class="row">
-                            <div class="col col-2 col-md-3 ml-4 ml-md-0">
-                                <div><i class="fa fa-money why-us-icon workin-process-icon"></i></div>
-                            </div>
-                            <div class="col col-8 col-md-9">
-                                <div>
-                                    <h5 class="why-us-icon-heading working-heading m-0">Best price guranteed</h5>
-                                    <p class="why-us-icon-paragraph working-steps-text pt-1">Find a lower price ? We'll refund you 100% of the difference .</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="mt-3">
-                        <div class="row">
-                            <div class="col col-2 col-md-3 ml-4 ml-md-0">
-                                <div><i class="fas fa-phone-volume why-us-icon workin-process-icon" style="text-align: center;"></i></div>
-                            </div>
-                            <div class="col col-8 col-md-9">
-                                <div>
-                                    <h5 class="why-us-icon-heading working-heading m-0">24/7 support service</h5>
-                                    <p class="why-us-icon-paragraph working-steps-text pt-1">We provide a 24 X 7 support service . please feel free to contact us .</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 </div>
