d0e85f304e7a9923fd537ff54ecfc3f7cb8ad4d5
ah-rasel <18103234@iubat.edu>
2022-02-04 21:48:07 +0600

    Car Brand Limit

--- app/Http/Livewire/AllTimeHitBrands.php
+++ app/Http/Livewire/AllTimeHitBrands.php
@@ -11,7 +11,7 @@ class AllTimeHitBrands extends Component
     public function render()
     {
         $brandsCount  = Brand::count();
-        $brands = Brand::with(['cars'])->take(6)->get();
-        return view('livewire.all-time-hit-brands',compact('brands','brandsCount'));
+        $brands = Brand::with(['cars'])->orderBy('id', 'DESC')->take(8)->get();
+        return view('livewire.all-time-hit-brands', compact('brands', 'brandsCount'));
     }
 }
